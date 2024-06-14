4c39d1d3829bf280c007343acb26eea554bedc29
username1a23 <139716060+username1a23@users.noreply.github.com>
2024-06-14 12:08:04 -0700

    Add files via upload

---
+++ HackathonGame/scripts/player.gd
@@ -0,0 +1,69 @@
+extends CharacterBody2D
+
+
+var enemy_inattack_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var player_alive = true
+
+const SPEED = 170.0
+const JUMP_VELOCITY = -350.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var jump_timer = 0.0
+
+
+
+@export var jump_buffer_time: float = .1
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump"):
+		jump_timer = 0.1
+	jump_timer -= delta
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		jump_timer = 0.0
+		velocity.y = JUMP_VELOCITY
+		
+
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+		
+	
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_player_hitbox_body_entered(body):
+	if body.has_method("enemy"):
+		enemy_inattack_range = true
+
+
+func _on_player_hitbox_body_exited(body):
+	pass # Replace with function body.
